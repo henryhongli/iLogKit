743e6df63bf3821f2c3146c83b8d065a6fe1da31
zhangyili <zhang_mao008@163.com>
2020-06-29 17:49:56 +0800

    code dev

--- iLogKit/Classes/LoganServer.swift
+++ iLogKit/Classes/LoganServer.swift
@@ -7,7 +7,7 @@
 //
 
 import Foundation
-import Logan
+//import Logan
 
 public class LoganServer {
     
@@ -31,49 +31,49 @@ public class LoganServer {
         let fileMax: uint_fast64_t = 100 * 1024 * 1024
         //日志有效期,1天
         let maxReversedDate: Int32 = 1
-        loganInit(keyData!, ivData!, fileMax)
-        loganSetMaxReversedDate(maxReversedDate)
-        loganUseASL(true)
+//        loganInit(keyData!, ivData!, fileMax)
+//        loganSetMaxReversedDate(maxReversedDate)
+//        loganUseASL(true)
     }
 
     ///上传日志
     public static func uploadAllLog(complete:((_ state:Bool)->())?){
         
-        if let info = Bundle.main.infoDictionary {
-            // 获取App的名称
-            let appId : String = info["CFBundleDisplayName"] as! String
-            let sysName = UIDevice.current.systemName //获取系统名称 例如：iPhone OS
-            let sysVersion = UIDevice.current.systemVersion //获取系统版本 例如：9.2
-            // 设备名称以及系统
-            let device = sysName + sysVersion
-            let deviceUUID = UIDevice.current.identifierForVendor?.uuid  //获取设备唯一标识符 例如：FBF2306E-A0D8-4F4B-BDED-9333B627D3E6
-            ///设备唯一标识
-            let unionIdStr = deviceUUID.debugDescription
-            ///日志时间
-            let now = Date()
-            let df = DateFormatter()
-            df.dateFormat = "yyyy-MM-dd"
-            let date = df.string(from: now)
-            loganUpload(loganUploadUrl, date, appId, unionIdStr, device) { (data, resp, error) in
-                if error != nil{
-                    print("upload error")
-                    print(error ?? "unknow error of upload log")
-                    complete?(false)
-                }else{
-                    ///上传成功
-                    if let res = resp {
-                        print("upload succeed")
-                        print(res)
-                    }
-                    complete?(false)
-                    ///上传成功清空所有日志
-                    loganClearAllLogs()
-                }
-            }
-        }else{
-            complete?(false)
-            print("获取本地InfoPlist文件失败")
-        }
+//        if let info = Bundle.main.infoDictionary {
+//            // 获取App的名称
+//            let appId : String = info["CFBundleDisplayName"] as! String
+//            let sysName = UIDevice.current.systemName //获取系统名称 例如：iPhone OS
+//            let sysVersion = UIDevice.current.systemVersion //获取系统版本 例如：9.2
+//            // 设备名称以及系统
+//            let device = sysName + sysVersion
+//            let deviceUUID = UIDevice.current.identifierForVendor?.uuid  //获取设备唯一标识符 例如：FBF2306E-A0D8-4F4B-BDED-9333B627D3E6
+//            ///设备唯一标识
+//            let unionIdStr = deviceUUID.debugDescription
+//            ///日志时间
+//            let now = Date()
+//            let df = DateFormatter()
+//            df.dateFormat = "yyyy-MM-dd"
+//            let date = df.string(from: now)
+//            loganUpload(loganUploadUrl, date, appId, unionIdStr, device) { (data, resp, error) in
+//                if error != nil{
+//                    print("upload error")
+//                    print(error ?? "unknow error of upload log")
+//                    complete?(false)
+//                }else{
+//                    ///上传成功
+//                    if let res = resp {
+//                        print("upload succeed")
+//                        print(res)
+//                    }
+//                    complete?(false)
+//                    ///上传成功清空所有日志
+//                    loganClearAllLogs()
+//                }
+//            }
+//        }else{
+//            complete?(false)
+//            print("获取本地InfoPlist文件失败")
+//        }
         
         
     }
@@ -84,7 +84,7 @@ public class LoganServer {
     }
     /// 设置是否需要在控制台打印日志
     public static func ifNeedPrint(_ ifNeed: Bool){
-        loganUseASL(ifNeed)
+//        loganUseASL(ifNeed)
     }
     
 }
@@ -140,7 +140,7 @@ extension ILOGAN where Base ==  ilogViewModel{
     public func write() {
         if base.logLevel.rawValue >= LoganServer.logLevelLimit.rawValue {
             /// 可以写入
-            logan(base.logLevel.rawValue, base.label)
+//            logan(base.logLevel.rawValue, base.label)
         }
     }
 }
